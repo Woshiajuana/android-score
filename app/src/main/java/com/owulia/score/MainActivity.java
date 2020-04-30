e341a82b4aec697a328802bf0278589cf3d0244b
Woshiajuana <zhigang.chen@owulia.com>
2020-04-30 14:13:14 +0800

    subcode code

--- app/src/main/java/com/owulia/score/MainActivity.java
+++ app/src/main/java/com/owulia/score/MainActivity.java
@@ -1,15 +1,15 @@
 package com.owulia.score;
 
+import android.os.Bundle;
+
+import com.owulia.score.databinding.ActivityMainBinding;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.SavedStateVMFactory;
 import androidx.lifecycle.ViewModelProviders;
 
-import android.content.Context;
-import android.content.SharedPreferences;
-import android.os.Bundle;
-
-import com.owulia.score.databinding.ActivityMainBinding;
+// ctl + alt + o 清楚不必要的引入
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,16 +20,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
-        myViewModel = ViewModelProviders.of(this,
-                new SavedStateVMFactory(this)).get(MyViewModel.class);
-//        myViewModel = new ViewModelProvider(this, new ViewModelProvider.NewInstanceFactory()).get(MyViewModel.class);
+        myViewModel = ViewModelProviders.of(this, new SavedStateVMFactory(this)).get(MyViewModel.class);
         binding.setData(myViewModel);
         binding.setLifecycleOwner(this);
-
-        SharedPreferences sharedPreferences = getPreferences(Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.putInt("NUMBER", 100);
-        editor.apply();
-
     }
+
 }
